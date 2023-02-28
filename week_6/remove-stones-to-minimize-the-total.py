4d3073111d04404a2add668489e11d678268d573
yunjeong <yunjeong.lee@day1company.co.kr>
2023-02-28 14:34:38 +0900

    함수로 변경

--- week_6/remove-stones-to-minimize-the-total.py
+++ week_6/remove-stones-to-minimize-the-total.py
@@ -1,10 +1,9 @@
 from heapq import heapify, heapreplace
 
-class Solution(object):
-    def minStoneSum(self, piles, k):
-        pq = [-x for x in piles]
-        heapify(pq)
-        for _ in range(k): heapreplace(pq, pq[0]//2)
-        return -sum(pq)
+def minStoneSum(piles, k):
+    pq = [-x for x in piles]
+    heapify(pq)
+    for _ in range(k): heapreplace(pq, pq[0]//2)
+    return -sum(pq)
     
-    print(minStoneSum(self, [5,4,9], 2))
+print(minStoneSum([5,4,9], 2))
